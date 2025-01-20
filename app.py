0b7187770d6105acc429e1a52afdfb31a51b0830
Ishwar Gautam <ishwargautam@lftechnology.com>
2025-01-20 11:57:30 +0545

    Use CKEDITOR for best content management.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'igtechteam'
+ckeditor = CKEditor(app)
 
 def get_db_connection():
     conn = sqlite3.connect('database/database.db')

d89d7f3279d2abc62180b0d8980598f553307408
Ishwar Gautam <ishwargautam@lftechnology.com>
2025-03-27 15:39:09 +0545

    Add summernote text editor.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
-from flask_ckeditor import CKEditor
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, url_for, flash, redirect, jsonify
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'igtechteam'
-ckeditor = CKEditor(app)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+# Ensure upload folder exists
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 def get_db_connection():
     conn = sqlite3.connect('database/database.db')
@@ -79,6 +83,21 @@ def delete(id):
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    
+    return jsonify({"url": url_for('static', filename=f'uploads/{filename}')})
 
 if __name__ == "__main__":
     app.run()

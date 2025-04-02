d5c895fea22d9490ad8833359795fe680ba6e249
Ishwar Gautam <ishwargautam@lftechnology.com>
2025-04-02 18:28:17 +0545

    Display note

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("login"))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run()
